3ba490307e2c5ff8674c161a405b723f6116ba48
hendry <h3ndry@users.noreply.github.com>
2023-09-28 14:16:48 +0200

    feat(stick-plot): first scarted plot of

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,45 @@
 <script>
     import data from "$data/data.js";
     import { scaleLinear } from "d3-scale";
+    import AxisX from "$components/AxisX.svelte";
+    import AxisY from "$components/AxisY.svelte";
 
-    console.log(data);
+    const margin = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 0,
+    };
 
     let width = 400;
     let height = 400;
-    let xScale = scaleLinear().domain([0, 100]).range([0, width]);
-    let yScale = scaleLinear().domain([0, 60]).range([height, 0]);
+    let innerWidth = width - margin.left - margin.right;
+    let innerHeight = height - margin.top - margin.bottom;
+    let xScale = scaleLinear().domain([0, 100]).range([0, innerWidth]);
+    let yScale = scaleLinear().domain([0, 60]).range([innerHeight, 0]);
 </script>
 
 <svg {width} {height}>
-    {#each data as d}
-        <circle
-            cx={xScale(d.grade)}
-            cy={yScale(d.hours)}
-            r={10}
-            fill="purple"
-            stroke="black"
-            stroke-width="1"
-        />
-    {/each}
+    <g transform="translate({margin.left}, {margin.top})">
+        <AxisX {xScale} width={innerWidth} height={innerHeight} />
+        <AxisY {yScale} width={innerWidth} />
+        {#each data as d}
+            <circle
+                cx={xScale(d.grade)}
+                cy={yScale(d.hours)}
+                r={10}
+                fill="purple"
+                stroke="black"
+                stroke-width="1"
+            />
+        {/each}
+    </g>
 </svg>
+
+<style>
+    :global(.tick text, .axis-title) {
+        fill: #8f8f8f;
+        font-weight: 400;
+        font-size: 14px;
+    }
+</style>
---
+++ src/components/AxisY.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { index } from "d3-array";
+
+    export let yScale;
+    export let width;
+    let yTicks = yScale.ticks(4);
+
+    console.log(yTicks);
+</script>
+
+{#each yTicks as tick, index}
+    <g class="tick" transform="translate(0,{yScale(tick)})">
+        <line
+            x1="0"
+            y1="0"
+            x2={width}
+            y2="0"
+            stroke={index == 0 ? "#8f8f8f" : "#e5e7eb"}
+        />
+
+        <text x="0" y="-6"
+            >{tick}{index === yTicks.length - 1 ? " hours studied" : ""}</text
+        >
+    </g>
+{/each}
