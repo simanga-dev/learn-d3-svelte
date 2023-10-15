a8539fa2a9bbcee365ca55020d6c557f1ff44205
hendry <h3ndry@users.noreply.github.com>
2023-10-15 07:16:15 +0200

    feat(story-telling): wip

--- src/routes/story-telling/+page.svelte
+++ src/routes/story-telling/+page.svelte
@@ -1,91 +1,53 @@
-<script lang='ts'>
-    import data from "./data";
-    import { scaleLinear } from "d3-scale";
-    import AxisX from "./AxisX.svelte";
-    import AxisY from "./AxisY.svelte";
-    import Tooltip from "./Tooltip.svelte";
+<script lang="ts">
+	import data from './data';
+	import { scaleLinear } from 'd3-scale';
+	// import AxisX from "./AxisX.svelte";
+	// import AxisY from "./AxisY.svelte";
+	// import Tooltip from "./Tooltip.svelte";
 
-    const margin = {
-        top: 20,
-        right: 20,
-        bottom: 20,
-        left: 15,
-    };
+	const margin = {
+		top: 20,
+		right: 20,
+		bottom: 20,
+		left: 15
+	};
 
-    let width = 400;
-    let height = 400;
-    $: innerWidth = width - margin.left - margin.right;
-    let innerHeight = height - margin.top - margin.bottom;
-    $: xScale = scaleLinear().domain([0, 100]).range([0, innerWidth]);
-    let yScale = scaleLinear().domain([0, 60]).range([innerHeight, 0]);
+	let width = 400;
+	let height = 400;
+	$: innerWidth = width - margin.left - margin.right;
+	let innerHeight = height - margin.top - margin.bottom;
+	$: xScale = scaleLinear().domain([0, 100]).range([0, innerWidth]);
+	let yScale = scaleLinear().domain([0, 60]).range([innerHeight, 0]);
 
-    let hoveredData;
-    let radius = 10;
+	let hoveredData;
+	let radius = 10;
 </script>
 
 <a class="text-blue-300" href="/">go back to charts</a>
 
 <section>
-
-<h1>student who study longer do well in exam</h1>
-<div class="chart-container" bind:clientWidth={width}>
-    <svg
-        {width}
-        {height}
-        on:mouseleave={() => {
-            hoveredData = "";
-        }}
-    >
-        <g transform="translate({margin.left}, {margin.top})">
-            <AxisX {xScale} width={innerWidth} height={innerHeight} />
-            <AxisY {yScale} width={innerWidth} />
-            {#each data.sort((a, b) => a.grade - b.grade) as d}
-                <circle
-                    role="button"
-                    cx={xScale(d.grade)}
-                    cy={yScale(d.hours)}
-                    r={hoveredData == d ? radius * 2 : radius}
-                    opacity={hoveredData ? (hoveredData == d ? 1 : 0.45) : 0.85}
-                    fill="purple"
-                    stroke="black"
-                    stroke-width="1"
-                    on:mouseover={() => {
-                        hoveredData = d;
-                    }}
-                    on:focus={() => {
-                        hoveredData = d;
-                    }}
-                    tabindex="0"
-                />
-            {/each}
-        </g>
-    </svg>
-
-    {#if hoveredData}
-        <Tooltip {xScale} {yScale} width={innerWidth} data={hoveredData} />
-    {/if}
-</div>
-
+	<h1>student who study longer do well in exam</h1>
+	<div class="chart-container" bind:clientWidth={width}>
+	</div>
 </section>
 
 <style>
-    :global(.tick text, .axis-title) {
-        fill: #8f8f8f;
-        font-weight: 400;
-        font-size: 14px;
-    }
+	:global(.tick text, .axis-title) {
+		fill: #8f8f8f;
+		font-weight: 400;
+		font-size: 14px;
+	}
 
-    .chart-container {
-        position: relative;
-    }
-    circle {
-        transition: r 300ms ease, opacity 500ms ease;
-        cursor: pointer;
-    }
-    h1 {
-        font-size: 1.3rem;
-        text-transform: capitalize;
-        margin-bottom: 0.8rem;
-    }
+	.chart-container {
+		position: relative;
+	}
+	circle {
+		transition: r 300ms ease, opacity 500ms ease;
+		cursor: pointer;
+	}
+	h1 {
+		font-size: 1.3rem;
+		text-transform: capitalize;
+		margin-bottom: 0.8rem;
+	}
 </style>
-
